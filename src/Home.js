414b318e2f1783a1927a6b4da77135ce6d14fb84
Demian Voksi <demian.voksi@gmail.com>
2021-10-25 21:37:02 +0200

    state

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = 'Mis';
+    const [name, setName] = useState('Mis');
+    const [age, setAge] = useState(3);
+
+    const handleClick = () => {
+        setName('Pigic');
+        setAge(3.5)
+    }
+    
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
