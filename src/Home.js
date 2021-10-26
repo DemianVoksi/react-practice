dc35d031dcd21512c95e53b811566b944bb451fa
Demian Voksi <demian.voksi@gmail.com>
2021-10-26 22:38:34 +0200

    props

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,15 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    // let name = 'Mis';
-    const [name, setName] = useState('Mis');
-    const [age, setAge] = useState(3);
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'Lorem ipsum...', author: 'Pigo', id: 1},
+        {title: 'Welcome party!', body: 'Lorem ipsum...', author: 'Mis', id: 2},
+        {title: 'Web dev top tips', body: 'Lorem ipsum...', author: 'Buco', id: 3}
+    ]);
 
-    const handleClick = () => {
-        setName('Pigic');
-        setAge(3.5)
-    }
-    
     return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-        </div>
+        <BlogList blogs={blogs} title="All blogs"/>
     );
 }
  
