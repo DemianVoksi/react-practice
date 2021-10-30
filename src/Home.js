a089238d8f01a19f3b0820a62effde0061ae4d5d
Demian Voksi <demian.voksi@gmail.com>
2021-10-30 22:05:04 +0200

    custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,18 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './UseFetch';
+
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(response => {
-                    if(!response.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return response.json()
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000);
-    }, []);
-
-    return (
-        <div className='home'>
-            { error && <div> { error }</div>}
-            { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All blogs"/>}
-        </div>  
-    );
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+  
+  return (
+    <div className='home'>
+      { error && <div> { error }</div>}
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All blogs"/> }
+    </div>  
+  );
 }
-
-
 
 export default Home;
