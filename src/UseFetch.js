ed7e86f89e5194f1323469987ffd5339ca3ca635
Demian Voksi <demian.voksi@gmail.com>
2021-10-31 16:33:18 +0100

    routing

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react'
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null); // react hook
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController(); 
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then(response => {
           if(!response.ok) {
             throw Error('could not fetch the data for that resource');
@@ -20,11 +22,16 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         })
     }, 1000);
-  }, []);
+    return () => abortCont.abort();
+  }, [url]);
 
   return { data, isPending, error };
 }
